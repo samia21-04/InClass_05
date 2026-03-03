280f04fabee0087c392addb954520335c599c8a9
Samia <Samiathaynes@gmail.com>
2026-03-02 20:10:55 -0500

    change: Update widget_test.dart files to reflect new app names

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:digital_pet_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const DigitalPetApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
